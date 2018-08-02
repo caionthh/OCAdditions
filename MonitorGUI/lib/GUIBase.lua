805cc2cec62f27976bc5f116961816d39d4b3cbf
caionthh <caionthh@hotmail.com>
2018-08-02 04:52:42 -0400

    GUIBase

---
+++ MonitorGUI/lib/GUIBase.lua
@@ -0,0 +1,14 @@
+
+--Requirements
+local colors = require "colors"
+
+--Version
+local _version = "0.1a"
+function ShowVersion()
+	print(_version)
+end
+function GetVersion()
+	return _version
+end
+
+return colors
